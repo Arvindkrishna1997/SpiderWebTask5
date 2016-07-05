3fa7013d187292fa2f17b0135093a698a882e512
Arvindkrishna1997 <arvindborn2win@gmail.com>
2016-07-05 06:20:40 +0530

    uploading task5

---
+++ Movies.js
@@ -0,0 +1,39 @@
+document.getElementById("form").addEventListener("submit",function(e){
+   e.preventDefault();
+
+  var  url="http://www.omdbapi.com/?t="+document.getElementById("Title").value+"&y="+document.getElementById("year").value+"&plot="+document.getElementById("plot").value+"&r=json";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+       // alert("came");
+        if(xhttp.status==200&&xhttp.readyState==4)
+        {
+            var array= JSON.parse(xhttp.responseText);
+            getDetails(array);
+        }
+    };
+    xhttp.open("GET",url,true);
+    xhttp.send();
+});
+function getDetails(array){       //A function the assign the datas. 
+    if(array.Response==="True") {
+        document.getElementById("movietitle").innerHTML = array.Title + "(" + array.Year + ")";
+        document.getElementById("imdbRating").innerHTML = array.imdbRating+"<sub>/10</sub>";
+        document.getElementById("imdbVotes").innerHTML = array.imdbVotes;
+        document.getElementById("Rated").innerHTML = array.Rated;
+        document.getElementById("Runtime").innerHTML = array.Runtime;
+        document.getElementById("Genre").innerHTML = array.Genre;
+        document.getElementById("Released").innerHTML = array.Released + "(" + array.Country + ")";
+        document.getElementById("Poster").src = array.Poster;
+        document.getElementById("plotofstory").innerHTML = array.Plot;
+        document.getElementById("Director").innerHTML = "<strong>Diector:</strong>" + array.Director;
+        document.getElementById("Writer").innerHTML = "<strong>Writer(s):</strong>" + array.Writer;
+        document.getElementById("Actors").innerHTML = "<strong>Actors:</strong>" + array.Actors;
+        document.getElementById("Language").innerHTML = "<strong>Language:</strong>" + array.Language;
+        document.getElementById("Awards").innerHTML = array.Awards;
+        document.getElementById("details").style.visibility = "";
+    }
+    else
+        alert("Movie not found");
+
+
+}
---
+++ comparemovies.js
@@ -0,0 +1,53 @@
+document.getElementById("form").addEventListener("submit",function(e){
+    e.preventDefault();
+
+    var  url1="http://www.omdbapi.com/?t="+document.getElementById("Title1").value+"&y=&plot=&r=json";
+    var  url2="http://www.omdbapi.com/?t="+document.getElementById("Title2").value+"&y=&plot=&r=json";
+    var array1,array2;
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        // alert("came");
+        if(xhttp.status==200&&xhttp.readyState==4)
+        {
+             array1= JSON.parse(xhttp.responseText);
+
+        }
+    };
+    xhttp.open("GET",url1,true);     //fetch movie1 details
+    xhttp.send();
+    var xhtp = new XMLHttpRequest();
+    xhtp.onreadystatechange = function(){
+        // alert("came");
+        if(xhtp.status==200&&xhtp.readyState==4)
+        {
+            array2= JSON.parse(xhtp.responseText);
+             getcompare(array1,array2);
+        }
+    };
+    xhtp.open("GET",url2,true);   //fetch movie2 details
+    xhtp.send();
+});
+function getcompare(array1,array2)   //function to compare
+{
+    if(array1.Response==="True"&&array2.Response==="True")
+    {    if(array1.imdbRating==="N/A")
+    {
+        document.getElementById("compare").innerHTML="ratings of "+array1.Title+" is not available";
+        return;
+    }
+     else if(array2.imdbRating==="N/A")
+    {
+        document.getElementById("compare").innerHTML="ratings of "+array2.Title+" is not available";
+        return;
+    }
+        if(array1.imdbRating>array2.imdbRating)
+        document.getElementById("compare").innerHTML= "<strong>"+array1.Title+"</strong>("+array1.imdbRating+"<sub>/10</sub>)  has more ratings than "+"<strong>"+array2.Title+"</strong>("+array2.imdbRating+"<sub>/10</sub>)";
+         else
+            document.getElementById("compare").innerHTML= "<strong>"+array2.Title+"</strong>("+array2.imdbRating+"<sub>/10</sub>)  has more ratings than "+"<strong>"+array1.Title+"</strong>("+array1.imdbRating+"<sub>/10</sub>)";
+    }
+    else
+        alert("Movie(s) not found");
+
+
+
+}
